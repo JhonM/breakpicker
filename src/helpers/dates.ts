3a41f266bb00ca995ac47dd55a4dcf04a285a7cf
JhonM <jhonmajoor@gmail.com>
2022-03-23 23:13:40 +0100

    Plural + More descriptive

--- src/helpers/dates.ts
+++ src/helpers/dates.ts
@@ -1,10 +1,23 @@
-const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-const date = new Date;
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const today = new Date();
 
 export const getYear = (d: Date) => d.getFullYear();
 export const getMonth = (d: Date) => d.getMonth();
-export const getMonthName = (d: Date) => month[d.getMonth()];
+export const getMonthName = (d: Date) => months[d.getMonth()];
 
-export const getCurrentYear = getYear(date);
-export const getCurrentMonth = getMonth(date);
-export const getCurrentMonthName = getMonthName(date);
+export const getCurrentYear = getYear(today);
+export const getCurrentMonth = getMonth(today);
+export const getCurrentMonthName = getMonthName(today);
