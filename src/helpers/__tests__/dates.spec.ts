9cb3a55fab3754816b6a29a6a47a6c80468e5538
JhonM <jhonmajoor@gmail.com>
2022-03-23 23:04:10 +0100

    Test all date related functions

--- src/helpers/__tests__/dates.spec.ts
+++ src/helpers/__tests__/dates.spec.ts
@@ -1,36 +1,65 @@
-import { getCurrentYear, getCurrentMonth, getCurrentMonthName } from "../dates";
+import {
+  getYear,
+  getMonth,
+  getMonthName,
+  getCurrentYear,
+  getCurrentMonth,
+  getCurrentMonthName,
+} from "../dates";
 
-describe("current dates", () => {
-  test("year", () => {
+describe("Get dates", () => {
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  test("getCurrentYear", () => {
     const currentYear = new Date();
 
     expect(getCurrentYear).toBe(currentYear.getFullYear());
   });
 
-  test("month", () => {
+  test("getCurrentMonth", () => {
     const currentMonth = new Date();
 
     expect(getCurrentMonth).toBe(currentMonth.getMonth());
   });
 
-  test("month name", () => {
-    const month = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
+  test("getCurrentMonthName", () => {
     const currentMonth = new Date();
     const expected = month[currentMonth.getMonth()];
 
     expect(getCurrentMonthName).toBe(expected);
+  });
+
+  test("getYear", () => {
+    const date = new Date("2016/02/16");
+    const expected = 2016;
+
+    expect(getYear(date)).toBe(expected);
+  });
+
+  test("getMonth", () => {
+    const date = new Date("2016/02/16");
+    const expected = 1;
+
+    expect(getMonth(date)).toBe(expected);
+  });
+
+  test("getMonthName", () => {
+    const date = new Date("2016/02/16");
+    const expected = month[date.getMonth()];
+
+    expect(getMonthName(date)).toBe(expected);
   });
 });
