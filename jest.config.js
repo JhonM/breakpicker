57db22f346c84fb77f7dedc0b60c9507a8cd1023
Jhon Majoor <jhonmajoor@gmail.com>
2023-06-10 23:58:46 +0200

    fix(): ts-jest vanilla extract css

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['<rootDir>/src/**/__tests__/**/*.spec.ts'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["<rootDir>/src/**/__tests__/**/*.spec.ts"],
+  transform: {
+    "\\.css\\.ts$": "@vanilla-extract/jest-transform",
+  },
 };
