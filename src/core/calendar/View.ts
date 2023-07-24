fbe13dcf60298495f17cae6db2b4910487f03eba
Jhon Majoor <jhonmajoor@gmail.com>
2023-07-24 15:36:31 +0200

    feat(): set cal to full width

--- src/core/calendar/View.ts
+++ src/core/calendar/View.ts
@@ -1,11 +1,12 @@
 import { h } from "@jhonm/blanc-vdom";
 import type { Model, DispatchType } from "../../types";
 import { CalendarView } from "./views";
+import { containerClass } from "../../styles/styles.css";
 
 export default function view(dispatch: DispatchType, model: Model) {
   return h(
     "div",
-    {},
+    { className: containerClass },
     ...[
       CalendarView(dispatch, model),
       h("pre", {}, JSON.stringify(model, null, 2)),
