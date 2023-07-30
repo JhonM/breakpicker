a4d4a2d75982f4e5fa6b76355fcaf3501ce3ebdd
Jhon Majoor <jhonmajoor@gmail.com>
2023-07-30 13:22:03 +0200

    feat(): use simple fieldset for title as example

--- src/core/calendar/views/addSlotFormView.ts
+++ src/core/calendar/views/addSlotFormView.ts
@@ -4,11 +4,26 @@ import { DispatchType, Model } from "../../../types";
 import { showAddFormMsg } from "../Update";
 
 function input(dispatch: DispatchType, model: Model) {
-  return h("input", { value: "null" });
+  return h("input", { type: "text", value: "model" });
+}
+
+function fieldSet(
+  dispatch: DispatchType,
+  model: Model,
+  input: (
+    dispatch: DispatchType,
+    model: Model
+  ) => { type: any; props: any; children: any[] }
+) {
+  return h("fieldset", {}, input(dispatch, model));
 }
 
 function closeButton(onclick: () => void) {
   return h("button", { onclick }, "close");
+}
+
+function titleFieldSet(dispatch: DispatchType, model: Model) {
+  return fieldSet(dispatch, model, input);
 }
 
 export function addSlotFormView(dispatch: DispatchType, model: Model) {
@@ -18,7 +33,10 @@ export function addSlotFormView(dispatch: DispatchType, model: Model) {
       {
         className: addSlotFormClass,
       },
-      ...[closeButton(() => dispatch(showAddFormMsg(false))), input]
+      ...[
+        closeButton(() => dispatch(showAddFormMsg(false))),
+        titleFieldSet(dispatch, model),
+      ]
     );
   }
 
