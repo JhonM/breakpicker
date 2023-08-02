1c41a34164a154a16ce8914fd793eee62f3eae24
Jhon Majoor <jhonmajoor@gmail.com>
2023-08-03 01:02:40 +0200

    feat(): add events to dates

--- src/core/calendar/views/monthView.ts
+++ src/core/calendar/views/monthView.ts
@@ -1,11 +1,4 @@
 import { h } from "@jhonm/blanc-vdom";
-import {
-  getYear,
-  getMonth,
-  getCalendarDays,
-  getCurrentMonth,
-  getCurrentYear,
-} from "../../../helpers/dates";
 import { DispatchType, Model } from "../../../types";
 import { selectedDayMsg } from "../Update";
 import { AddSlotView } from "./";
@@ -22,93 +15,7 @@ function dayContainer(...props: any[]) {
   return h("div", { className: dayContainerClass }, ...props);
 }
 
-const selectedDate = (day: number) =>
-  new Date(`${getCurrentYear}-${getCurrentMonth + 1}-${day}`);
-
 export function monthView(dispatch: DispatchType, model: Model) {
-  /*
-  const daysArray = getCalendarDays().reduce((acc: any, i) => {
-    const currentDate = model.currentDate;
-
-    const prevLastDay = new Date(
-      getYear(currentDate),
-      getMonth(currentDate),
-      0
-    ).getDate();
-    const totalMonthDay = new Date(
-      getYear(currentDate),
-      getMonth(currentDate) + 1,
-      0
-    ).getDate();
-    const startWeekDay = new Date(
-      getYear(currentDate),
-      getMonth(currentDate),
-      1
-    ).getDay();
-
-    const getEventDate = (model: Model) => (day: Date) =>
-      model.events?.find((e) => e?.date.getDate() === day.getDate());
-    const day = i - startWeekDay;
-    let month;
-
-    const selectedDateDay = selectedDate(day);
-    const t = getEventDate(model)(selectedDateDay);
-
-    const withSlots = t ? t : "";
-
-    console.info(withSlots);
-    if (i <= startWeekDay) {
-      month = dayContainer(
-        h(
-          "div",
-          {
-            className: `${dayClass} ${prevLastDayClass} `,
-            onclick: () => console.log("clicked prev last day"),
-          },
-          `${prevLastDay - i}`
-        )
-      );
-    } else if (i <= startWeekDay + totalMonthDay) {
-      currentDate.setDate(day);
-      currentDate.setHours(0, 0, 0, 0);
-
-      const currentDayOrMonthDay =
-        currentDate.getTime() === new Date().setHours(0, 0, 0, 0)
-          ? currentDayClass
-          : monthDayClass;
-
-      month = dayContainer(
-        h(
-          "div",
-          {
-            className: `${dayClass} ${currentDayOrMonthDay}`,
-            onclick: () => dispatch(selectedDayMsg(day)),
-          },
-          ...[day.toString(), withSlots.toString()]
-        ),
-        AddSlotView(dispatch, model)
-      );
-    } else {
-      month = dayContainer(
-        h(
-          "div",
-          {
-            className: dayClass,
-            onclick: () => console.log("clicked day view else"),
-          },
-          `${day - totalMonthDay}`
-        )
-      );
-    }
-
-    if (Array.isArray(acc)) {
-      acc.push(month);
-    }
-
-    return acc;
-  }, []);
-*/
-
   const { year, month } = model;
   const firstDay = new Date(year, month, 1);
   const lastDay = new Date(year, month + 1, 0);
@@ -118,12 +25,12 @@ export function monthView(dispatch: DispatchType, model: Model) {
   const day = firstDay.getDay();
   const nextDays = 7 - lastDay.getDay() - 1;
   const daysArray = [];
+  const selectedDate = (day: number) =>
+    new Date(`${model.year}-${model.month + 1}-${day}`);
 
-  const getEventDate = (model: Model) => (day: Date) =>
-    model.events?.find((e) => e?.date.getDate() === day.getDate());
-
-  const selectedDateDay = selectedDate(day);
-  const t = getEventDate(model)(selectedDateDay);
+  const findSlots = model.events
+    ?.filter((event) => event.slots)
+    .filter((e) => e.date);
 
   let x = day;
   while (x > 0) {
@@ -133,6 +40,7 @@ export function monthView(dispatch: DispatchType, model: Model) {
         {
           className: `${dayClass} ${prevLastDayClass} `,
           onclick: () => console.log("clicked prev last day"),
+          "data-day": `${prevDays - x + 1}`,
         },
         `${prevDays - x + 1}`
       )
@@ -156,22 +64,30 @@ export function monthView(dispatch: DispatchType, model: Model) {
           {
             className: `${dayClass} ${currentDayClass}`,
             onclick: () => dispatch(selectedDayMsg(day)),
+            "data-day": `${i}`,
           },
           `${i}`
-        )
-        // AddSlotView(dispatch, model)
+        ),
+        AddSlotView(dispatch, model)
       );
 
       daysArray.push(view);
     } else {
+      const slots = findSlots?.filter(
+        (event) =>
+          event.date.toLocaleDateString() ===
+          selectedDate(i).toLocaleDateString()
+      );
+
       const view = dayContainer(
         h(
           "div",
           {
             className: `${dayClass} ${monthDayClass}`,
             onclick: () => dispatch(selectedDayMsg(day)),
+            "data-day": `${i}`,
           },
-          ...[`${i}`]
+          ...[`${i}`, `${slots}`, AddSlotView(dispatch, model)]
         )
       );
 
@@ -188,6 +104,7 @@ export function monthView(dispatch: DispatchType, model: Model) {
         {
           className: dayClass,
           onclick: () => console.log("clicked day view else"),
+          "data-day": `${j}`,
         },
         `${j}`
       )
