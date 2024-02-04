20c6d43ba2642cdf5f4b062acccc5ef3c051e55e
Jhon Majoor <jhonmajoor@gmail.com>
2024-02-04 10:57:01 +0100

    refactor(): general even slot fn

--- src/core/calendar/views/events/eventView.ts
+++ src/core/calendar/views/events/eventView.ts
@@ -3,9 +3,9 @@ import { DispatchType, EventType } from "../../../../types";
 import { eventClass } from "../../../../styles/styles.css";
 import { SlotsView } from "../slots";
 
-export function eventView(dispatch: DispatchType, event: EventType) {
-  if (!event.slots) {
-    return h("div", { className: eventClass }, "add new slot");
+export function eventView(dispatch: DispatchType, event?: EventType) {
+  if (!event?.slots) {
+    return h("div", {});
   }
 
   return h("div", { className: eventClass }, SlotsView(dispatch, event.slots));
