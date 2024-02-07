96f163d85774d27b17e6b2ef2ecc75c91cdc5e19
Jhon Majoor <jhonmajoor@gmail.com>
2024-02-07 15:13:00 +0100

    fix(): add event on static id

--- src/core/calendar/views/addSlotFormView.ts
+++ src/core/calendar/views/addSlotFormView.ts
@@ -3,20 +3,20 @@ import { addSlotFormClass } from "../../../styles/styles.css";
 import { DispatchType, Model } from "../../../types";
 import { showAddFormMsg, onSubmitMsg } from "../Update";
 
-function input(dispatch: DispatchType, model: Model, name: string) {
-  return h("input", { type: "text", value: "model", name }, "");
+function input(dispatch: DispatchType, name: string) {
+  return h("input", { type: "text", name }, "");
 }
 
-function fieldSet(dispatch: DispatchType, model: Model, name: string) {
-  return h("fieldset", {}, input(dispatch, model, name));
+function fieldSet(dispatch: DispatchType, name: string) {
+  return h("fieldset", {}, input(dispatch, name));
 }
 
 function closeButton(onclick: () => void) {
   return h("button", { onclick }, "close");
 }
 
-function titleFieldSet(dispatch: DispatchType, model: Model, name: string) {
-  return fieldSet(dispatch, model, name);
+function baseInput(dispatch: DispatchType, name: string) {
+  return fieldSet(dispatch, name);
 }
 
 function submitButton() {
@@ -33,14 +33,25 @@ export function addSlotFormView(dispatch: DispatchType, model: Model) {
         className: addSlotFormClass,
         onsubmit: (e: SubmitEvent) => {
           e.preventDefault();
-          dispatch(onSubmitMsg());
+
+          const title = e.target?.title.value;
+          const duration = e.target?.duration.value;
+
+          dispatch(
+            onSubmitMsg({
+              title,
+              duration,
+              date,
+            })
+          );
           dispatch(showAddFormMsg(false));
         },
       },
       ...[
         closeButton(() => dispatch(showAddFormMsg(false))),
-        titleFieldSet(dispatch, model, "title"),
-        `${date.toLocaleDateString()}`,
+        baseInput(dispatch, "title"),
+        baseInput(dispatch, "duration"),
+        `${date.toLocaleDateString()} jhonln`,
         submitButton(),
       ]
     );
