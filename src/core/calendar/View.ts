91e4bad74a1e3732102a4f39c8c277816b92c18e
Jhon Majoor <jhonmajoor@gmail.com>
2024-08-28 23:50:38 +0200

    feat: adds confirm dialog

--- src/core/calendar/View.ts
+++ src/core/calendar/View.ts
@@ -1,7 +1,7 @@
 import { h } from "@jhonm/blanc-vdom";
 import type { Model, DispatchType } from "../../types";
 import { CalendarView, AddSlotFormView, ToastView } from "./views";
-import { containerClass } from "../../styles/styles.css";
+import { containerClass, dialogClass } from "../../styles/styles.css";
 import { undoAddLatestSlotMsg } from "./update/updateUndoLatestSlot";
 import "./views/dialog-off-canvas";
 
@@ -24,6 +24,25 @@ export default function view(dispatch: DispatchType, model: Model) {
         "dialog-off-canvas",
         { heading: "From base view", open: model.showForm ? true : false },
         AddSlotFormView(dispatch, model)
+      ),
+      h(
+        "dialog",
+        {
+          id: "confirm-dialog",
+          className: dialogClass,
+          onclose: (e: Event) => {
+            const target = e.target;
+            const dialogContent = (target as HTMLFormElement)?.querySelector(
+              "form"
+            );
+            if (!dialogContent) return;
+            dialogContent.remove();
+          },
+        },
+        ...[
+          h("slot", { name: "confirm-dialog-heading" }),
+          h("slot", { name: "confirm-dialog-content" }),
+        ]
       ),
       h("pre", {}, JSON.stringify(model, null, 2)),
     ]
--- src/core/calendar/views/slots/slotView.ts
+++ src/core/calendar/views/slots/slotView.ts
@@ -1,24 +1,104 @@
-import { h } from "@jhonm/blanc-vdom";
+import { h, render } from "@jhonm/blanc-vdom";
 import { DispatchType, EventType, Model, Slot } from "../../../../types";
 import { deleteSlotMsg } from "../../update/updateDeleteSlot";
 import { currentSlotIdMsg } from "../../update/updateCurrentSlotId";
 import { setEventsBeforeCRUD } from "../../update/updateSetEventsBeforeCRUD";
 import { slotTitle } from "./slotTitle";
+import { guid } from "../../../../helpers/random";
 
-function slotDeleteButton(
-  dispatch: DispatchType,
-  id: Slot["id"],
-  startDate: Slot["startDate"],
-  model: Model
-) {
+type SlotViewProp = {
+  dispatch: DispatchType;
+  id: Slot["id"];
+  title: Slot["title"];
+  startDate: Slot["startDate"];
+  model: Model;
+};
+
+function slotDeleteButton({
+  dispatch,
+  id,
+  title,
+  startDate,
+  model,
+}: SlotViewProp) {
+  const formId = guid();
+  const handleClose = ({
+    dialog,
+    id,
+  }: {
+    dialog: HTMLDialogElement;
+    id: string;
+  }) => {
+    const form = document.getElementById(id);
+    dialog.close();
+    form?.remove();
+  };
+
   return h(
     "div",
     {
       onclick: (e: Event) => {
         e.stopPropagation();
-        dispatch(setEventsBeforeCRUD(model.events));
-        dispatch(deleteSlotMsg(id, startDate));
-        dispatch(currentSlotIdMsg(id));
+
+        const dialog = document.getElementById(
+          "confirm-dialog"
+        ) as HTMLDialogElement;
+
+        const dialogContent = render(
+          h(
+            "form",
+            {
+              method: "dialog",
+              id: formId,
+              onsubmit: (e: SubmitEvent) => {
+                e.stopPropagation();
+                dispatch(setEventsBeforeCRUD(model.events));
+                dispatch(deleteSlotMsg(id, startDate));
+                dispatch(currentSlotIdMsg(id));
+              },
+            },
+            ...[
+              h(
+                "button",
+                { onclick: () => handleClose({ dialog, id: formId }) },
+                "X"
+              ),
+              h(
+                "H1",
+                {
+                  slot: "confirm-dialog-heading",
+                },
+                `Delete "${title}"`
+              ),
+              h(
+                "p",
+                {
+                  slot: "confirm-dialog-content",
+                },
+                ...[
+                  h(
+                    "button",
+                    {
+                      onclick: () => handleClose({ dialog, id: formId }),
+                    },
+                    "No"
+                  ),
+                  h(
+                    "button",
+                    {
+                      type: "submit",
+                    },
+                    "Yes"
+                  ),
+                ]
+              ),
+            ]
+          )
+        );
+
+        if (!dialog) return;
+        dialog.appendChild(dialogContent);
+        dialog.showModal();
       },
     },
     "X"
@@ -37,7 +117,7 @@ export function slotView(
     {},
     ...[
       slotTitle({ dispatch, title, id, model, eventId }),
-      slotDeleteButton(dispatch, id, startDate, model),
+      slotDeleteButton({ dispatch, title, id, startDate, model }),
     ]
   );
 }
